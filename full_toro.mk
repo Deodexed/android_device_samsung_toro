a9b4384e4b023c2b48c39c1763cb03ab0cddd436
Deodexed <Deodexed@gmail.com>
2011-12-25 12:37:18 -0500

    Update full_toro.mk

--- full_toro.mk
+++ full_toro.mk
@@ -21,13 +21,7 @@
 # lines, full and toro, hence its name.
 #
 
-# Camera
-PRODUCT_PACKAGES := \
-    Camera \
-    Gallery \
-    Superuser \
-    Superuser.apk \
-    su
+
 
 #if we do this after the full_base_telephony is included some of these don't get picked up..
 PRODUCT_COPY_FILES += \
